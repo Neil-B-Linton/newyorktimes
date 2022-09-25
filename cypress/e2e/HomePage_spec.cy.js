8298cc83448de821ad0e53f25ef3faec089efe15
Neil Linton <lintonbneil@gmail.com>
2022-09-24 19:50:43 -0700

    Add cypress testing, and stubbing

--- cypress/e2e/HomePage_spec.cy.js
+++ cypress/e2e/HomePage_spec.cy.js
@@ -1,5 +1,16 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('HomePage', () => {
+  beforeEach('a user should see a home page', () => {
+    cy.visit('http://localhost:3000/')
+    cy.intercept('GET', 'https://api.nytimes.com/svc/topstories/v2/us.json?api-key=YUB09xwrCV2grcQuBoT36bRu9oKBTLNW')
+
+  })
+  it('Should display a title to the user', () => {
+    cy.get('h1').should('be.visible')
+  })
+  it('Should title should be clickable', () => {
+    cy.get('h1').click()
+  })
+  it('Should display cards to the user', () => {
+    cy.get('.Link').should('be.visible')
   })
 })
